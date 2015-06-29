4e9b54f5fac59f509c73aabdd767d7a6c30bd857
jholsapple <japple1000@yahoo.com>
2015-06-29 12:22:36 -0400

    New app created
    Iron events

---
+++ IronEvents/IronEvents/EventsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventsTableViewController.h
+//  IronEvents
+//
+//  Created by John Holsapple on 6/29/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventsTableViewController : UITableViewController
+
+@end
---
+++ IronEvents/IronEvents/SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  IronEvents
+//
+//  Created by John Holsapple on 6/29/15.
+//  Copyright (c) 2015 John Holsapple -- The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+
+@end
